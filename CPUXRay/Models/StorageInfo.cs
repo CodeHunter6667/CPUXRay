d4e6c34d4ec85d0dbdb8ea4bb68e5cad1a60beb9
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-09-12 21:23:38 -0300

    feat: criado classes de Readers dos Models

--- CPUXRay/Models/StorageInfo.cs
+++ CPUXRay/Models/StorageInfo.cs
@@ -13,40 +13,4 @@ public class StorageInfo
     public ulong FreeSpaceGB { get; set; }
     public float ReadSpeedMBps { get; set; }
     public float WriteSpeedMBps { get; set; }
-
-    private static List<StorageInfo> GetStorageInfo()
-    {
-        var drivers = new List<StorageInfo>();
-
-        foreach (var drive in DriveInfo.GetDrives())
-        {
-            if (drive.IsReady && drive.DriveType == DriveType.Fixed)
-            {
-                var info = new StorageInfo
-                {
-                    DriverLetter = drive.Name,
-                    TotalSpaceGB = (ulong)(drive.TotalSize / (1024 * 1024 * 1024)),
-                    FreeSpaceGB = (ulong)(drive.TotalFreeSpace / (1024 * 1024 * 1024)),
-                };
-                drivers.Add(info);
-            }
-        }
-        return drivers;
-    }
-
-    public static void EnrichStorageInfo(List<StorageInfo> drives)
-    {
-        using var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive");
-        var diskIndex = 0;
-
-        foreach (var disk in searcher.Get())
-        {
-            if (diskIndex < drives.Count)
-            {
-                drives[diskIndex].Model = disk["Model"]?.ToString() ?? "Unknown";
-                drives[diskIndex].Type = disk["MediaType"]?.ToString() ?? "Unknown";
-            }
-            diskIndex++;
-        }
-    }
 }
---
+++ CPUXRay/Readers/GpuReader.cs
@@ -0,0 +1,60 @@
+﻿using CPUXRay.Models;
+using LibreHardwareMonitor.Hardware;
+
+namespace CPUXRay.Readers;
+
+public static class GpuReader
+{
+    public static GpuInfo GetGpuInfo()
+    {
+        var gpu = new GpuInfo();
+        var computer = new Computer
+        {
+            IsGpuEnabled = true
+        };
+
+        computer.Open();
+
+        foreach (var hardware in computer.Hardware)
+        {
+            if (hardware.HardwareType == HardwareType.GpuNvidia || hardware.HardwareType == HardwareType.GpuAmd || hardware.HardwareType == HardwareType.GpuIntel)
+            {
+                hardware.Update();
+                gpu.Name = hardware.Name;
+                gpu.Manufacturer = hardware.HardwareType.ToString().Replace("Gpu", "");
+
+                foreach (var sensor in hardware.Sensors)
+                {
+                    switch (sensor.SensorType)
+                    {
+                        case SensorType.Temperature when sensor.Name.Contains("GPU Core"):
+                            gpu.TemperatureCelsius = sensor.Value ?? 0;
+                            break;
+
+                        case SensorType.Load when sensor.Name.Contains("GPU Core"):
+                            gpu.UsagePercentage = sensor.Value ?? 0;
+                            break;
+
+                        case SensorType.Clock when sensor.Name.Contains("GPU Core"):
+                            gpu.CoreClockMHz = sensor.Value ?? 0;
+                            break;
+
+                        case SensorType.Clock when sensor.Name.Contains("GPU Memory"):
+                            gpu.MemoryClockMHz = sensor.Value ?? 0;
+                            break;
+
+                        case SensorType.SmallData when sensor.Name.Contains("GPU Memory Total"):
+                            gpu.TotalMemoryMB = (ulong)(sensor.Value ?? 0);
+                            break;
+
+                        case SensorType.SmallData when sensor.Name.Contains("GPU Memory Used"):
+                            gpu.UsedMemoryMB = (ulong)(sensor.Value ?? 0);
+                            break;
+                    }
+                }
+            }
+        }
+        computer.Close();
+        return gpu;
+    }
+}
