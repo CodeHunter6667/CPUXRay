f552c3209f6e5d346f4ef4c2384101219eaa5e45
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-09-14 18:03:45 -0300

    fix: ajustado serviço de monitoramento de hardware

--- CPUXRay/Services/HardwareMonitorService.cs
+++ CPUXRay/Services/HardwareMonitorService.cs
@@ -1,17 +1,21 @@
 ﻿using CPUXRay.Models;
+using Microsoft.UI.Dispatching;
 using Microsoft.UI.Xaml;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace CPUXRay.Services;
 
 public class HardwareMonitorService
 {
     private readonly DispatcherTimer _timer;
+    private readonly DispatcherQueue _dispatcherQueue;
     private readonly Action<CpuInfo, RamInfo, GpuInfo, List<StorageInfo>, MotherboardInfo> _onUpdate;
 
-    public HardwareMonitorService(Action<CpuInfo, RamInfo, GpuInfo, List<StorageInfo>, MotherboardInfo> onUpdateCallback, int intervalSeconds = 2)
+    public HardwareMonitorService(DispatcherQueue dispatcherQueue, Action<CpuInfo, RamInfo, GpuInfo, List<StorageInfo>, MotherboardInfo> onUpdateCallback, int intervalSeconds = 2)
     {
+        _dispatcherQueue = dispatcherQueue;
         _onUpdate = onUpdateCallback;
 
         _timer = new DispatcherTimer
@@ -26,12 +30,25 @@ public class HardwareMonitorService
 
     private void Timer_Tick(object sender, object e)
     {
-        var cpu = CpuService.GetCpuInfo();
-        var ram = RamService.GetRamInfo();
-        var gpu = GpuService.GetGpuInfo();
-        var storage = StorageService.GetStorageInfo();
-        var motherboard = MotherboardService.GetMotherboardInfo();
+        Task.Run(() =>
+        {
+            var cpuTask = Task.Run(() => CpuService.GetCpuInfo());
+            var ramTask = Task.Run(() => RamService.GetRamInfo());
+            var gpuTask = Task.Run(() => GpuService.GetGpuInfo());
+            var storageTask = Task.Run(() => StorageService.GetStorageInfo());
+            var boardTask = Task.Run(() => MotherboardService.GetMotherboardInfo());
+            Task.WaitAll(cpuTask, ramTask, gpuTask, storageTask, boardTask);
 
-        _onUpdate?.Invoke(cpu, ram, gpu, storage, motherboard);
+            var cpu = cpuTask.Result;
+            var ram = ramTask.Result;
+            var gpu = gpuTask.Result;
+            var storage = storageTask.Result;
+            var board = boardTask.Result;
+
+            _dispatcherQueue.TryEnqueue(() =>
+            {
+                _onUpdate?.Invoke(cpu, ram, gpu, storage, board);
+            });
+        });
     }
 }
