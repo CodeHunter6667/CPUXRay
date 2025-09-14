88955bb41fb35cf799975fc06038d5d497dabc1c
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-09-14 19:08:45 -0300

    Finalizado HardwareMonitorService e criado MainViewModel (#3)
    * feat: terminados os métodos de mapeamento das informações do hardware
    
    * feat: criado as classes de Service
    
    * feat: criado classes de Readers dos Models
    
    * feat: finalizado classes Services
    
    * feat: finalizado serviço de monitoramento de hardware
    
    * fix: ajustado serviço de monitoramento de hardware
    
    * feat: criado MainViewModel

---
+++ CPUXRay/Services/HardwareMonitorService.cs
@@ -0,0 +1,54 @@
+﻿using CPUXRay.Models;
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CPUXRay.Services;
+
+public class HardwareMonitorService
+{
+    private readonly DispatcherTimer _timer;
+    private readonly DispatcherQueue _dispatcherQueue;
+    private readonly Action<CpuInfo, RamInfo, GpuInfo, List<StorageInfo>, MotherboardInfo> _onUpdate;
+
+    public HardwareMonitorService(DispatcherQueue dispatcherQueue, Action<CpuInfo, RamInfo, GpuInfo, List<StorageInfo>, MotherboardInfo> onUpdateCallback, int intervalSeconds = 2)
+    {
+        _dispatcherQueue = dispatcherQueue;
+        _onUpdate = onUpdateCallback;
+
+        _timer = new DispatcherTimer
+        {
+            Interval = TimeSpan.FromSeconds(intervalSeconds)
+        };
+        _timer.Tick += Timer_Tick;
+    }
+
+    public void Start() => _timer.Start();
+    public void Stop() => _timer.Stop();
+
+    private void Timer_Tick(object sender, object e)
+    {
+        Task.Run(() =>
+        {
+            var cpuTask = Task.Run(() => CpuService.GetCpuInfo());
+            var ramTask = Task.Run(() => RamService.GetRamInfo());
+            var gpuTask = Task.Run(() => GpuService.GetGpuInfo());
+            var storageTask = Task.Run(() => StorageService.GetStorageInfo());
+            var boardTask = Task.Run(() => MotherboardService.GetMotherboardInfo());
+            Task.WaitAll(cpuTask, ramTask, gpuTask, storageTask, boardTask);
+
+            var cpu = cpuTask.Result;
+            var ram = ramTask.Result;
+            var gpu = gpuTask.Result;
+            var storage = storageTask.Result;
+            var board = boardTask.Result;
+
+            _dispatcherQueue.TryEnqueue(() =>
+            {
+                _onUpdate?.Invoke(cpu, ram, gpu, storage, board);
+            });
+        });
+    }
+}
---
+++ CPUXRay/ViewModel/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using CPUXRay.Models;
+using CPUXRay.Services;
+using Microsoft.UI.Dispatching;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CPUXRay.ViewModel;
+
+public class MainViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public CpuInfo Cpu { get; set; } = new();
+    public RamInfo Ram { get; set; } = new RamInfo();
+    public GpuInfo Gpu { get; set; } = new();
+    public List<StorageInfo> Storage { get; set; } = new();
+    public MotherboardInfo Motherboard { get; set; } = new();
+
+    private HardwareMonitorService _monitorService;
+
+    public MainViewModel()
+    {
+        var dispatcherQueue = DispatcherQueue.GetForCurrentThread();
+        _monitorService = new HardwareMonitorService(dispatcherQueue, OnHardwareUpdated);
+        _monitorService.Start();
+    }
+
+    private void OnHardwareUpdated(CpuInfo cpu, RamInfo ram, GpuInfo gpu, List<StorageInfo> storage, MotherboardInfo board)
+    {
+        Cpu = cpu;
+        Ram = ram;
+        Gpu = gpu;
+        Storage = storage;
+        Motherboard = board;
+        OnPropertyChanged(nameof(Cpu));
+        OnPropertyChanged(nameof(Ram));
+        OnPropertyChanged(nameof(Gpu));
+        OnPropertyChanged(nameof(Storage));
+        OnPropertyChanged(nameof(Motherboard));
+    }
+
+    protected void OnPropertyChanged([CallerMemberName] string name = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
